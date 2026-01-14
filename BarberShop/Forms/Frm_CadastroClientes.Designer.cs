3d0fd368fe3e21d3aa54c28639ece6c127a9302c
Juan Antony <juanantony.nascimento5@gmail.com>
2026-01-14 18:20:16 -0300

    Renomeando os campos para melhor distinção na hora de implementar a lógica

--- BarberShop/Forms/Frm_CadastroClientes.Designer.cs
+++ BarberShop/Forms/Frm_CadastroClientes.Designer.cs
@@ -28,171 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_CadastroClientes));
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
-			this.textBox2 = new System.Windows.Forms.TextBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.checkBox1 = new System.Windows.Forms.CheckBox();
-			this.label3 = new System.Windows.Forms.Label();
-			this.label4 = new System.Windows.Forms.Label();
-			this.textBox3 = new System.Windows.Forms.TextBox();
-			this.label5 = new System.Windows.Forms.Label();
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
-			this.dataGridView1 = new System.Windows.Forms.DataGridView();
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.checkBox2 = new System.Windows.Forms.CheckBox();
-			this.pictureBox2 = new System.Windows.Forms.PictureBox();
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// textBox1
-			// 
-			resources.ApplyResources(this.textBox1, "textBox1");
-			this.textBox1.Name = "textBox1";
-			// 
-			// maskedTextBox1
-			// 
-			resources.ApplyResources(this.maskedTextBox1, "maskedTextBox1");
-			this.maskedTextBox1.Name = "maskedTextBox1";
-			// 
-			// textBox2
-			// 
-			resources.ApplyResources(this.textBox2, "textBox2");
-			this.textBox2.Name = "textBox2";
-			// 
-			// label1
-			// 
-			resources.ApplyResources(this.label1, "label1");
-			this.label1.BackColor = System.Drawing.Color.Transparent;
-			this.label1.ForeColor = System.Drawing.Color.White;
-			this.label1.Name = "label1";
-			this.label1.Click += new System.EventHandler(this.label1_Click);
-			// 
-			// checkBox1
-			// 
-			resources.ApplyResources(this.checkBox1, "checkBox1");
-			this.checkBox1.BackColor = System.Drawing.Color.Transparent;
-			this.checkBox1.ForeColor = System.Drawing.Color.White;
-			this.checkBox1.Name = "checkBox1";
-			this.checkBox1.UseVisualStyleBackColor = false;
-			// 
-			// label3
-			// 
-			resources.ApplyResources(this.label3, "label3");
-			this.label3.BackColor = System.Drawing.Color.Transparent;
-			this.label3.ForeColor = System.Drawing.Color.White;
-			this.label3.Name = "label3";
-			// 
-			// label4
-			// 
-			resources.ApplyResources(this.label4, "label4");
-			this.label4.BackColor = System.Drawing.Color.Transparent;
-			this.label4.ForeColor = System.Drawing.Color.White;
-			this.label4.Name = "label4";
-			// 
-			// textBox3
-			// 
-			resources.ApplyResources(this.textBox3, "textBox3");
-			this.textBox3.Name = "textBox3";
-			// 
-			// label5
-			// 
-			resources.ApplyResources(this.label5, "label5");
-			this.label5.BackColor = System.Drawing.Color.Transparent;
-			this.label5.ForeColor = System.Drawing.Color.White;
-			this.label5.Name = "label5";
-			// 
-			// button1
-			// 
-			resources.ApplyResources(this.button1, "button1");
-			this.button1.Name = "button1";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// button2
-			// 
-			resources.ApplyResources(this.button2, "button2");
-			this.button2.Name = "button2";
-			this.button2.UseVisualStyleBackColor = true;
-			// 
-			// dataGridView1
-			// 
-			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			resources.ApplyResources(this.dataGridView1, "dataGridView1");
-			this.dataGridView1.Name = "dataGridView1";
-			// 
-			// pictureBox1
-			// 
-			this.pictureBox1.BackColor = System.Drawing.Color.Black;
-			resources.ApplyResources(this.pictureBox1, "pictureBox1");
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.TabStop = false;
-			// 
-			// checkBox2
-			// 
-			resources.ApplyResources(this.checkBox2, "checkBox2");
-			this.checkBox2.BackColor = System.Drawing.Color.Transparent;
-			this.checkBox2.ForeColor = System.Drawing.Color.White;
-			this.checkBox2.Name = "checkBox2";
-			this.checkBox2.UseVisualStyleBackColor = false;
-			this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
-			// 
-			// pictureBox2
-			// 
-			this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-			resources.ApplyResources(this.pictureBox2, "pictureBox2");
-			this.pictureBox2.Name = "pictureBox2";
-			this.pictureBox2.TabStop = false;
-			// 
-			// Form1
-			// 
-			resources.ApplyResources(this, "$this");
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.SystemColors.Control;
-			this.Controls.Add(this.pictureBox2);
-			this.Controls.Add(this.checkBox2);
-			this.Controls.Add(this.pictureBox1);
-			this.Controls.Add(this.dataGridView1);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.label5);
-			this.Controls.Add(this.textBox3);
-			this.Controls.Add(this.label4);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.checkBox1);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.textBox2);
-			this.Controls.Add(this.maskedTextBox1);
-			this.Controls.Add(this.textBox1);
-			this.Name = "Form1";
-			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_CadastroClientes));
+            txt_nome = new TextBox();
+            txt_email = new MaskedTextBox();
+            txt_telefone = new TextBox();
+            label1 = new Label();
+            cbx_ativo = new CheckBox();
+            label3 = new Label();
+            label2 = new Label();
+            txt_cpf = new TextBox();
+            label4 = new Label();
+            btn_salvar = new Button();
+            btn_deletar = new Button();
+            dtg_banco = new DataGridView();
+            pictureBox1 = new PictureBox();
+            cbx_inativo = new CheckBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)dtg_banco).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // txt_nome
+            // 
+            resources.ApplyResources(txt_nome, "txt_nome");
+            txt_nome.Name = "txt_nome";
+            // 
+            // txt_email
+            // 
+            resources.ApplyResources(txt_email, "txt_email");
+            txt_email.Name = "txt_email";
+            // 
+            // txt_telefone
+            // 
+            resources.ApplyResources(txt_telefone, "txt_telefone");
+            txt_telefone.Name = "txt_telefone";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.BackColor = Color.Transparent;
+            label1.ForeColor = Color.White;
+            label1.Name = "label1";
+            // 
+            // cbx_ativo
+            // 
+            resources.ApplyResources(cbx_ativo, "cbx_ativo");
+            cbx_ativo.BackColor = Color.Transparent;
+            cbx_ativo.ForeColor = Color.White;
+            cbx_ativo.Name = "cbx_ativo";
+            cbx_ativo.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(label3, "label3");
+            label3.BackColor = Color.Transparent;
+            label3.ForeColor = Color.White;
+            label3.Name = "label3";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(label2, "label2");
+            label2.BackColor = Color.Transparent;
+            label2.ForeColor = Color.White;
+            label2.Name = "label2";
+            // 
+            // txt_cpf
+            // 
+            resources.ApplyResources(txt_cpf, "txt_cpf");
+            txt_cpf.Name = "txt_cpf";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(label4, "label4");
+            label4.BackColor = Color.Transparent;
+            label4.ForeColor = Color.White;
+            label4.Name = "label4";
+            // 
+            // btn_salvar
+            // 
+            resources.ApplyResources(btn_salvar, "btn_salvar");
+            btn_salvar.Name = "btn_salvar";
+            btn_salvar.UseVisualStyleBackColor = true;
+            // 
+            // btn_deletar
+            // 
+            resources.ApplyResources(btn_deletar, "btn_deletar");
+            btn_deletar.Name = "btn_deletar";
+            btn_deletar.UseVisualStyleBackColor = true;
+            // 
+            // dtg_banco
+            // 
+            dtg_banco.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(dtg_banco, "dtg_banco");
+            dtg_banco.Name = "dtg_banco";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Black;
+            resources.ApplyResources(pictureBox1, "pictureBox1");
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.TabStop = false;
+            // 
+            // cbx_inativo
+            // 
+            resources.ApplyResources(cbx_inativo, "cbx_inativo");
+            cbx_inativo.BackColor = Color.Transparent;
+            cbx_inativo.ForeColor = Color.White;
+            cbx_inativo.Name = "cbx_inativo";
+            cbx_inativo.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            resources.ApplyResources(pictureBox2, "pictureBox2");
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.TabStop = false;
+            // 
+            // Frm_CadastroClientes
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            Controls.Add(pictureBox2);
+            Controls.Add(cbx_inativo);
+            Controls.Add(pictureBox1);
+            Controls.Add(dtg_banco);
+            Controls.Add(btn_deletar);
+            Controls.Add(btn_salvar);
+            Controls.Add(label4);
+            Controls.Add(txt_cpf);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(cbx_ativo);
+            Controls.Add(label1);
+            Controls.Add(txt_telefone);
+            Controls.Add(txt_email);
+            Controls.Add(txt_nome);
+            Name = "Frm_CadastroClientes";
+            ((System.ComponentModel.ISupportInitialize)dtg_banco).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.MaskedTextBox txt_email;
+        private System.Windows.Forms.TextBox txt_telefone;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox cbx_ativo;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_cpf;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_salvar;
+        private System.Windows.Forms.Button btn_deletar;
+        private System.Windows.Forms.DataGridView dtg_banco;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox cbx_inativo;
         private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
--- BarberShop/Forms/Frm_CadastroClientes.cs
+++ BarberShop/Forms/Frm_CadastroClientes.cs
@@ -16,25 +16,5 @@ namespace projeto
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBox2_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
     }
 }
